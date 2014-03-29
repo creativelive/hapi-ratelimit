b1d8cc08d4969490cf7574c2d8a58eb6db459296
cl-aaron <aaron.elligsen@creativelive.com>
2014-03-29 15:04:10 +0000

    Forgot to include the tests

---
+++ tests/limit.js
@@ -0,0 +1,22 @@
+var request = require('supertest');
+describe("Hapi route based rate limiting", function() {
+  var url = "http://localhost:8081";
+  it("Should return 429 if local limit is reached",function(done) {
+    request(url).get('/test').expect(200, function() {
+      request(url).get('/test').expect(200, function() {
+        request(url).get('/test').expect(429, done);
+      });
+    });
+  });
+
+  it("Should allow new requests after time period and then return 429 if local limit is reached",function(done) {
+    this.timeout(10*1000);
+    setTimeout(function() {
+      request(url).get('/test').expect(200, function() {
+        request(url).get('/test').expect(200, function() {
+          request(url).get('/test').expect(429, done);
+        });
+      });
+    }, 7*1000);
+  });
+});
---
+++ tests/testserver.js
@@ -0,0 +1,54 @@
+var Hapi = require('hapi');
+var ansi = require('simple-ansi');
+var moment = require('moment');
+
+var debug = true;
+var server = new Hapi.Server('localhost', 8081, {});
+server.route([{
+  method: "GET",
+  path:"/test",
+  handler: function(request, reply) {
+      reply({success:"Test Ok!"});
+  },
+  config:{
+    plugins:{"hapi-ratelimit":{limit:2, bucketLength: 5}}
+  }
+},{
+  method: "GET",
+  path:"/testglobal",
+  handler: function(request, reply) {
+      reply({success:"Test Ok!"});
+  },
+}]);
+
+var rateopts = {redis:{port:6379, host:'localhost'}, global: {limit:10, bucketLength: 20}};
+server.pack.require('../../hapi-ratelimit',rateopts, function(err){
+  if(err) {
+    console.log(err);
+  }
+});
+
+server.on('request', function(request, event, tags) {
+  'use strict';
+  var now, time;
+
+  if (debug === true) {
+    now = moment().format('HH:mm:ss');
+    time = ansi.bold + ansi.gray + now + ansi.reset;
+    console.log(time, '[' + ansi.green + request.method.toUpperCase() + ansi.reset
+      + ']', ansi.blue + ansi.bold + request.path + ansi.reset);
+    if (request.params && request.params.length > 0) {
+      console.log(ansi.yellow,'\tParams: ', request.params, ansi.reset);
+    }
+    if (request.payload) {
+      console.log(ansi.cyan,'\tPayload: ', request.payload, ansi.reset);
+    }
+    if (request.query && Object.keys(request.query).length > 0) {
+      console.log(ansi.magenta,'\tQuery: ', request.query, ansi.reset);
+    }
+  }
+});
+
+server.start(function() {
+  console.log("Hapi Test Server Started")
+});
